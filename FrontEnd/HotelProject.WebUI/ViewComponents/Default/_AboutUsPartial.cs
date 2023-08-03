5424f288b79e36d5fe20f3837a73ae1e14823ccb
mcelik99 <mehmetcelik.99@outlook.com>
2023-08-04 00:35:50 +0300

    About Consume

--- FrontEnd/HotelProject.WebUI/ViewComponents/Default/_AboutUsPartial.cs
+++ FrontEnd/HotelProject.WebUI/ViewComponents/Default/_AboutUsPartial.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.AboutDto;
+using HotelProject.WebUI.Models.Staff;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace HotelProject.WebUI.ViewComponents.Default
 {
     public class _AboutUsPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        IHttpClientFactory _httpClientFactory;
+
+        public _AboutUsPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5000/api/About");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
