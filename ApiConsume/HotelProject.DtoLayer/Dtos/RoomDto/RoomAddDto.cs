7fd548a0c73f620ba891ffe03bbeff0c92c2e0cf
mcelik99 <mehmetcelik.99@outlook.com>
2023-08-03 11:27:17 +0300

    Add RoomDto and Room2Controller

--- ApiConsume/HotelProject.DtoLayer/Dtos/RoomDto/RoomAddDto.cs
+++ ApiConsume/HotelProject.DtoLayer/Dtos/RoomDto/RoomAddDto.cs
@@ -1,22 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HotelProject.EntityLayer.Concrete
+namespace HotelProject.DtoLayer.Dtos.RoomDto
 {
-    public class Room
+    public class RoomAddDto
     {
-        public int RoomId { get; set; }
+        [Required(ErrorMessage ="Lütfen oda numarasını yazın")]
         public string RoomNumber { get; set; }
         public string RoomCoverImage { get; set; }
+        [Required(ErrorMessage = "Lütfen fiyat bilgisi yazın")]
         public int Price { get; set; }
+        [Required(ErrorMessage = "Lütfen oda başlığını  yazın")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Lütfen yatak sayısını yazın")]
         public string BedCount { get; set; }
+        [Required(ErrorMessage = "Lütfen banyo sayısını yazın")]
         public string BathCount { get; set; }
         public string Wifi { get; set; }
         public string Description { get; set; }
-
     }
 }
---
+++ ApiConsume/HotelProject.DtoLayer/Dtos/RoomDto/UpdateRoomDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DtoLayer.Dtos.RoomDto
+{
+    public class UpdateRoomDto
+    {
+        public int RoomId { get; set; }
+        [Required(ErrorMessage = "Lütfen oda numarasını yazın")]
+        public string RoomNumber { get; set; }
+        [Required(ErrorMessage = "Lütfen oda görseli girin")]
+        public string RoomCoverImage { get; set; }
+        [Required(ErrorMessage = "Lütfen fiyat bilgisi yazın")]
+        public int Price { get; set; }
+        [Required(ErrorMessage = "Lütfen oda başlığını  yazın")]
+        [StringLength(100,ErrorMessage = " Lütfen en fazla 100 karakter girişi yapın")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Lütfen yatak sayısını yazın")]
+        public string BedCount { get; set; }
+        [Required(ErrorMessage = "Lütfen banyo sayısını yazın")]
+        public string BathCount { get; set; }
+        public string Wifi { get; set; }
+        [Required(ErrorMessage = "Lütfen açıklamayı girin yazın")]
+        public string Description { get; set; }
+    }
+}
