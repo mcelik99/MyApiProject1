6a6dbc17fa7af557d1a25b26aead4a04d6e1c0eb
mcelik99 <mehmetcelik.99@outlook.com>
2023-08-03 16:10:58 +0300

    Add ServiceDto and ServiceController

---
+++ FrontEnd/HotelProject.WebUI/Dtos/ServiceDto/CreateServiceDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.ServiceDto
+{
+    public class CreateServiceDto
+    {
+        [Required(ErrorMessage = " Service icon linki giriniz")]
+        public string ServiceIcon { get; set; }
+        [Required(ErrorMessage = " Hizmet başlığı giriniz")]
+        [StringLength(100,ErrorMessage = " Hizmet balığı en fazla 100 karakter olabilir")]
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
