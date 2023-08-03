d08fe23bfe7a3243b684c0531eb3d3f774766cc8
mcelik99 <mehmetcelik.99@outlook.com>
2023-08-03 23:31:54 +0300

    Front-End Temasının ViewComponentlere Ayrılması

---
+++ FrontEnd/HotelProject.WebUI/ViewComponents/Default/_AboutUsPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace HotelProject.WebUI.ViewComponents.Default
+{
+    public class _AboutUsPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
