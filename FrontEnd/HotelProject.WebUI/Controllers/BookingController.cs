a81e0d655e36ae7126dad0a181382a4da6b50391
mcelik99 <mehmetcelik.99@outlook.com>
2023-08-04 19:35:05 +0300

    Booking İşlemi

--- FrontEnd/HotelProject.WebUI/Controllers/BookingController.cs
+++ FrontEnd/HotelProject.WebUI/Controllers/BookingController.cs
@@ -1,10 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.BookingDto;
+using HotelProject.WebUI.Dtos.SubscribeDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace HotelProject.WebUI.Controllers
 {
     public class BookingController : Controller
     {
+        IHttpClientFactory _httpClientFactory;
+
+        public BookingController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
         public IActionResult Index()
         {
             return View();
@@ -15,8 +26,17 @@ namespace HotelProject.WebUI.Controllers
             return PartialView();
         }
         [HttpPost]
-        public async Task<IActionResult> AddBooking(string x) 
+        public async Task<IActionResult> AddBooking(CreateBookingDto   createBookingDto) 
         {
+            createBookingDto.Status = "Onay Bekliyor";
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createBookingDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("http://localhost:5000/api/Booking ", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
