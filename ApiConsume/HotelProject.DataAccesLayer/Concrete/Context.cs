c7172bd62c423ca3df492e507d286cd053575d43
mcelik99 <mehmetcelik.99@outlook.com>
2023-08-08 21:45:23 +0300

    Add Guest

--- ApiConsume/HotelProject.DataAccesLayer/Concrete/Context.cs
+++ ApiConsume/HotelProject.DataAccesLayer/Concrete/Context.cs
@@ -30,5 +30,6 @@ namespace HotelProject.DataAccesLayer.Concrete
 
         public DbSet<About> Abouts { get; set; }
         public DbSet<Booking>  Bookings { get; set; }
+        public DbSet<Guest>  Guests { get; set; }
     }
 }
