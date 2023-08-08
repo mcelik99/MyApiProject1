9420708fbf9e9635dab7de247fc7ab1c0a43db06
mcelik99 <mehmetcelik.99@outlook.com>
2023-08-08 13:52:27 +0300

    Booking Api consume

---
+++ RapidApi/RapidApiConsume/Controllers/BookingController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RapidApiConsume.Models;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace RapidApiConsume.Controllers
+{
+    public class BookingController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<BookingApiViewModel> bookingApiViewModels = new List<BookingApiViewModel>();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/hotels/search?checkin_date=2023-08-10&dest_type=city&units=metric&checkout_date=2023-08-13&adults_number=2&order_by=popularity&dest_id=-1456928&filter_by_currency=AED&locale=en-gb&room_number=1&children_number=2&children_ages=5%2C0&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&page_number=0&include_adjacency=true"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "ac1cddd525msh33304df8e31f6bbp1011d4jsncf14a527d2fd" },
+        { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<BookingApiViewModel>(body);
+                return View(values.result.ToList());
+            }
+           
+        }
+    }
+}
