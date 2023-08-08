0674c238cca444a5bf891fdff83599f2e0be1be1
mcelik99 <mehmetcelik.99@outlook.com>
2023-08-08 10:22:01 +0300

    Rapid Api Döviz Kuru Çekme

---
+++ RapidApi/RapidApiConsume/Controllers/ExchanceController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using RapidApiConsume.Models;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace RapidApiConsume.Controllers
+{
+    public class ExchanceController : Controller
+    {
+        public async Task< IActionResult> Index()
+        {
+           List<ExchanceViewModel> exchanceViewModels = new List<ExchanceViewModel>();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/metadata/exchange-rates?currency=TRY&locale=en-gb"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "ac1cddd525msh33304df8e31f6bbp1011d4jsncf14a527d2fd" },
+        { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values= JsonConvert.DeserializeObject<ExchanceViewModel>(body);
+                return View(values.exchange_rates.ToList());
+            }
+            
+        }
+    }
+}
