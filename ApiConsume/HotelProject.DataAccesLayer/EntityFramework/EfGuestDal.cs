db5ab374c7a383084276686a638c4a22186565b5
mcelik99 <mehmetcelik.99@outlook.com>
2023-08-10 12:15:29 +0300

    Guest İşlemleri

---
+++ ApiConsume/HotelProject.DataAccesLayer/EntityFramework/EfGuestDal.cs
@@ -0,0 +1,21 @@
+﻿using HotelProject.DataAccesLayer.Abstract;
+using HotelProject.DataAccesLayer.Concrete;
+using HotelProject.DataAccesLayer.Repositories;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccesLayer.EntityFramework
+{
+    public class EfGuestDal : GenericRepository<Guest>,IGuestDal
+    {
+        public EfGuestDal(Context context) : base(context)
+        { 
+
+        }
+
+    }
+}
