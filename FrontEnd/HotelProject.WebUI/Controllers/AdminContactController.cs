dee4da337448edaed5da86859640259be8e8a60e
mcelik99 <mehmetcelik.99@outlook.com>
2023-08-10 18:49:16 +0300

    Admin Yeni Mesaj Gönderme İşlemi

--- FrontEnd/HotelProject.WebUI/Controllers/AdminContactController.cs
+++ FrontEnd/HotelProject.WebUI/Controllers/AdminContactController.cs
@@ -1,10 +1,12 @@
 ﻿using HotelProject.WebUI.Dtos.ContactDto;
+using HotelProject.WebUI.Dtos.SendMessageDto;
 using HotelProject.WebUI.Models.Staff;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace HotelProject.WebUI.Controllers
@@ -55,6 +57,28 @@ namespace HotelProject.WebUI.Controllers
                 return View("Error", ex); 
             }
         }
+        [HttpGet]
+        public IActionResult AddSendMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddSendMessage(CreateSendMessageDto  createSendMessageDto)
+        {
+            createSendMessageDto.SenderMail = "admin@gmail.com";
+            createSendMessageDto.SenderName = "admin";
+            createSendMessageDto.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createSendMessageDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("http://localhost:5000/api/SendMessage", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("SendBox");
+            }
+            return View();
+        }
+
         public PartialViewResult SideBarAdminContactPartial()
         {
             return PartialView();
