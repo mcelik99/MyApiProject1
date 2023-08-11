1c79f11725cc7e8b03c091eedbb14d7778f8bb95
mcelik99 <mehmetcelik.99@outlook.com>
2023-08-11 16:31:47 +0300

    Proje Seviyesinde Authorize

--- FrontEnd/HotelProject.WebUI/Startup.cs
+++ FrontEnd/HotelProject.WebUI/Startup.cs
@@ -4,8 +4,10 @@ using HotelProject.DataAccesLayer.Concrete;
 using HotelProject.EntityLayer.Concrete;
 using HotelProject.WebUI.Dtos.GuestDto;
 using HotelProject.WebUI.ValidationRules.GuestValidationRules;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -36,7 +38,16 @@ namespace HotelProject.WebUI
             services.AddTransient<IValidator<UpdateGuestDto>, UpdateGuestValidator>();
             services.AddControllersWithViews().AddFluentValidation();
 
+
             services.AddAutoMapper(typeof(Startup));
+
+            services.AddMvc(config =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .Build();
+                config.Filters.Add(new AuthorizeFilter(policy));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +62,7 @@ namespace HotelProject.WebUI
                 app.UseExceptionHandler("/Home/Error");
             }
             app.UseStaticFiles();
+            app.UseAuthentication();
 
             app.UseRouting();
 
