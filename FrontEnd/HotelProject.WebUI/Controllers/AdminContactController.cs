8b9f682d84073555a9731f5fe9b3ba725c44d7a0
mcelik99 <mehmetcelik.99@outlook.com>
2023-08-11 22:52:14 +0300

    GetContactCount Consume

--- FrontEnd/HotelProject.WebUI/Controllers/AdminContactController.cs
+++ FrontEnd/HotelProject.WebUI/Controllers/AdminContactController.cs
@@ -27,10 +27,22 @@ namespace HotelProject.WebUI.Controllers
                 var client = _httpClientFactory.CreateClient();
                 var responseMessage = await client.GetAsync("http://localhost:5000/api/Contact");
 
+                var client2 = _httpClientFactory.CreateClient();
+                var responseMessage2 = await client2.GetAsync("http://localhost:5000/api/Contact/GetContactCount");
+
+                var client3 = _httpClientFactory.CreateClient();
+                var responseMessage3 = await client3.GetAsync("http://localhost:5000/api/Contact/GetContactCount");
+
                 if (responseMessage.IsSuccessStatusCode)
                 {
                     var jsonData = await responseMessage.Content.ReadAsStringAsync();
                     var values = JsonConvert.DeserializeObject<List<InboxContactDto>>(jsonData);
+
+                    var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
+                    ViewBag.contactCount = jsonData2;
+
+                    var jsonData3 = await responseMessage3.Content.ReadAsStringAsync();
+                    ViewBag.sendMessageCount = jsonData3;
 
                     if (values != null)
                     {
