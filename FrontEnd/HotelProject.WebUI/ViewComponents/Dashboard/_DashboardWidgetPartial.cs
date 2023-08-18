623f0939ea50db8f557c82e32c7fd68cf64f1fe3
mcelik99 <mehmetcelik.99@outlook.com>
2023-08-18 17:23:14 +0300

    StaffCount Consume

--- FrontEnd/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardWidgetPartial.cs
+++ FrontEnd/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardWidgetPartial.cs
@@ -1,11 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.GuestDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace HotelProject.WebUI.ViewComponents.Dashboard
 {
     public class _DashboardWidgetPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DashboardWidgetPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task< IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5000/api/DashboardWidgets/StaffCount");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                //var values = JsonConvert.DeserializeObject<List<ResultGuestDto>>(jsonData);
+                ViewBag.v = jsonData;
+            }
             return View();
         }
     }
